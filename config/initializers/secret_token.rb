27ad985a12695e68ec40fcdd1d565197970c88ce
Manuel <manu@batkiwi.com>
2012-11-11 00:22:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moose::Application.config.secret_token = '3c382cee5453285362842b57e50375c526e0051b6e53c25cbaf821c49d3363f735386b333460c75360ffacb47fd34d29c894fffc42b8e73a986ef75ad8a7d4fd'
